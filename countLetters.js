5ea51137a895e341fae8f4196dd00b59e50e825e
Emily K <emily.ke@hotmail.com>
2021-09-15 18:07:51 -0400

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertation Failed: [${actual}] !=== [${expected}]`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const finalAnswer = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (!(finalAnswer[sentence[i]])) {
+      finalAnswer[sentence[i]] = 1;
+    } else
+      finalAnswer[sentence[i]]++;
+  }
+  return finalAnswer;
+};
+
+// TEST CODE
+console.log(countLetters("lighthouse"));
