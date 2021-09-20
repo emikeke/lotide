4f1b1ddb74b5c36624003665a5423609902c6526
Emily K <emily.ke@hotmail.com>
2021-09-20 14:28:57 -0400

    refactor lotide assignment

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertation Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertation Failed: [${actual}] !=== [${expected}]`);
-  }
-};
-
 const tail = function(input) {
   let finalArray = [];
   for (let i = 1; i < input.length; i++) {
@@ -16,8 +7,4 @@ const tail = function(input) {
   return finalArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-tail([]);
+module.exports = tail;
