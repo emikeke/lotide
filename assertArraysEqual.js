43d05901842f5121c3332da9bd557f2fd5b2853b
Emily K <emily.ke@hotmail.com>
2021-09-20 14:51:05 -0400

    refactor lotide assignment

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,7 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function(input, input2) {
-  if (input.length !== input2.length) {
-    return false;
-  }
-  let arrayEqual = true;
-  for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
-    if (input[inputIndex] === input2[inputIndex]) {
-      arrayEqual &= true;
-    } else
-      arrayEqual &= false;
-  }
-  console.log((Boolean(arrayEqual)));
-  return (Boolean(arrayEqual));
+  return eqArrays(input, input2);
 };
+
+module.exports = assertArraysEqual;
