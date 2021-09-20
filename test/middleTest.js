ffde7762189fd00de4e46c40570ef4ce689be046
Emily K <emily.ke@hotmail.com>
2021-09-20 16:46:21 -0400

    refactor lotide assignment w mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,28 @@
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+describe ("#middle", () => {
+  it ("returns []", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it ("returns []", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it ("returns [2]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it ("returns [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it ("returns [3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it ("returns [3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,19 @@
+const assert = require("chai").assert;
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
-// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-tail([]);
+
+describe ("#tail", () => {
+  it ("no need to capture return value since we are not checking it", () => {
+    console.log(tail(words));
+  });
+
+  it ("original array should still have 3 elements", () => {
+    assertEqual(words.length, 3);
+  });
+
+  it("returns ' ' for []", () => {
+    return;
+  });
+});
